cea4a54b689bbf7c3230ef1d7b34775beaeb1c1d
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2019-06-14 22:48:48 -0300

    Conclusão do App

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -1,11 +1,116 @@
 import React, { Component } from "react";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
+import ImagePicker from "react-native-image-picker";
+import RNFS from "react-native-fs";
+import FileViewer from "react-native-file-viewer";
+import socket from "socket.io-client";
 
-import { View } from "react-native";
+import { distanceInWords } from "date-fns";
+import pt from "date-fns/locale/pt";
 
-// import { Container } from './styles';
+import api from "../../services/api";
+
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+import styles from "./styles";
 
 export default class Box extends Component {
+  state = { box: {} };
+
+  async componentDidMount() {
+    const box = await AsyncStorage.getItem("@RocketBox:box");
+
+    this.subscribeToNewFiles(box);
+
+    const response = await api.get(`boxes/${box}`);
+
+    this.setState({ box: response.data });
+  }
+
+  subscribeToNewFiles = box => {
+    const io = socket("https://node-rocketbox.herokuapp.com");
+
+    io.emit("connectRoom", box);
+
+    io.on("file", data => {
+      this.setState({
+        box: { ...this.state.box, files: [data, ...this.state.box.files] }
+      });
+    });
+  };
+
+  openFile = async file => {
+    try {
+      const filePath = `${RNFS.DocumentDirectoryPath}/${file.title}`;
+
+      await RNFS.downloadFile({
+        fromUrl: file.url,
+        toFile: filePath
+      });
+
+      await FileViewer.open(filePath);
+    } catch (err) {
+      console.log("Arquivo não suportado");
+    }
+  };
+
+  handleUpload = () => {
+    ImagePicker.launchImageLibrary({}, async upload => {
+      if (upload.error) {
+        console.log("ImagePicker error");
+      } else if (upload.didCancel) {
+        console.log("Canceled by user");
+      } else {
+        const data = new FormData();
+
+        const [prefix, suffix] = upload.fileName.split(".");
+        const ext = suffix.toLowerCase() === "heic" ? ".jpg" : suffix;
+
+        data.append("file", {
+          uri: upload.uri,
+          type: upload.type,
+          name: `${prefix}.${ext}`
+        });
+
+        api.post(`boxes/${this.state.box._id}/files`, data);
+      }
+    });
+  };
+
+  renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => this.openFile(item)} style={styles.file}>
+      <View style={styles.fileInfo}>
+        <Icon name="insert-drive-file" size={24} color="#A5CFFF" />
+        <Text style={styles.fileTitle}>{item.title}</Text>
+      </View>
+
+      <Text style={styles.fileDate}>
+        há{" "}
+        {distanceInWords(item.createdAt, new Date(), {
+          locale: pt
+        })}
+      </Text>
+    </TouchableOpacity>
+  );
+
   render() {
-    return <View />;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.boxTitle}>{this.state.box.title}</Text>
+
+        <FlatList
+          styles={styles.list}
+          data={this.state.box.files}
+          keyExtractor={file => file._id}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          renderItem={this.renderItem}
+        />
+
+        <TouchableOpacity style={styles.fab} onPress={this.handleUpload}>
+          <Icon name="cloud-upload" size={24} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    );
   }
 }
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-
 import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
 
 import api from "../../services/api";
 
@@ -13,10 +13,20 @@ export default class Main extends Component {
     newBox: ""
   };
 
+  async componentDidMount() {
+    const box = await AsyncStorage.getItem("@RocketBox:box");
+
+    if (box) {
+      this.props.navigation.navigate("Box");
+    }
+  }
+
   handleSignIn = async () => {
     const response = await api.post("boxes", {
       title: this.state.newBox
     });
+
+    await AsyncStorage.setItem("@RocketBox:box", response.data._id);
 
     this.props.navigation.navigate("Box");
   };
